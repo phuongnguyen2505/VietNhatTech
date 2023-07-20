1eda55fb4239e48b5d05087de99c8454f49b8027
Phuong Nguyen <phuong.t13579@gmail.com>
2023-07-20 15:37:12 +0700

    constant

--- admin/lib/constant.php
+++ admin/lib/constant.php
@@ -1,97 +1,98 @@
-<?php if(!@defined('_lib')) die("Error");
-	
-	
-	
-	@define ( _updating , "Đang cập nhật thông tin" );
-	
-	@define ( _upload_dichvu, '../upload/dichvu/' );
-	@define ( _upload_dichvu_l , 'upload/dichvu/' );
-	@define ( _upload_khachhang, '../upload/khachhang/' );
-	@define ( _upload_khachhang_l , 'upload/khachhang/' );
-	@define ( _upload_raovat, '../upload/raovat/' );
-	@define ( _upload_raovat_l , 'upload/raovat/' );
-	@define ( _upload_gioithieu, '../upload/gioithieu/' );
-	@define ( _upload_gioithieu_l , 'upload/gioithieu/' );
-	@define ( _upload_hinhanhquangcao, '../upload/hinhanhquangcao/' );
-	@define ( _upload_hinhanhquangcao_l , 'upload/hinhanhquangcao/' );
-	@define ( _upload_duan, '../upload/duan/' );
-	@define ( _upload_duan_l , 'upload/duan/' );
-	@define ( _upload_tintuc , '../upload/news/' );
-	@define ( _upload_tintuc_l , 'upload/news/' );
-	
-	@define ( _upload_file , '../upload/' );
-	@define ( _upload_file_l , 'upload/' );
-	
-	@define ( _upload_download , '../upload/download/' );
-	@define ( _upload_download_l , 'upload/download/' );
+<?php if(!@defined('_lib')) { die("Error");
+}
+    
+    
+    
+    @define(_updating, "Đang cập nhật thông tin");
+    define('_updating', true);
+    @define(_upload_dichvu, '../upload/dichvu/');
+    @define(_upload_dichvu_l, 'upload/dichvu/');
+    @define(_upload_khachhang, '../upload/khachhang/');
+    @define(_upload_khachhang_l, 'upload/khachhang/');
+    @define(_upload_raovat, '../upload/raovat/');
+    @define(_upload_raovat_l, 'upload/raovat/');
+    @define(_upload_gioithieu, '../upload/gioithieu/');
+    @define(_upload_gioithieu_l, 'upload/gioithieu/');
+    @define(_upload_hinhanhquangcao, '../upload/hinhanhquangcao/');
+    @define(_upload_hinhanhquangcao_l, 'upload/hinhanhquangcao/');
+    @define(_upload_duan, '../upload/duan/');
+    @define(_upload_duan_l, 'upload/duan/');
+    @define(_upload_tintuc, '../upload/news/');
+    @define(_upload_tintuc_l, 'upload/news/');
+    
+    @define(_upload_file, '../upload/');
+    @define(_upload_file_l, 'upload/');
+    
+    @define(_upload_download, '../upload/download/');
+    @define(_upload_download_l, 'upload/download/');
 
-	@define ( _upload_hinhanh , '../upload/hinhanh/' );
-	@define ( _upload_hinhanh_l , 'upload/hinhanh/' );
-	
-	@define ( _upload_thuvienanh , '../upload/thuvienanh/' );
-	@define ( _upload_thuvienanh_l , 'upload/thuvienanh/' );
+    @define(_upload_hinhanh, '../upload/hinhanh/');
+    @define(_upload_hinhanh_l, 'upload/hinhanh/');
+    
+    @define(_upload_thuvienanh, '../upload/thuvienanh/');
+    @define(_upload_thuvienanh_l, 'upload/thuvienanh/');
 
-	@define ( _upload_album , '../upload/slider/' );
-	@define ( _upload_album_l , 'upload/slider/' );
-	
-	@define ( _upload_product , '../upload/product/' );
-	@define ( _upload_product_l , 'upload/product/' );
+    @define(_upload_album, '../upload/slider/');
+    @define(_upload_album_l, 'upload/slider/');
+    
+    @define(_upload_product, '../upload/product/');
+    @define(_upload_product_l, 'upload/product/');
 
-	
-	#======================================= kich thuoc hinh anh
-	
-	@define ( _news_thumb_w , 75 );
-	@define ( _news_thumb_h , 83 );
-	@define ( _news_photo_w , 120 );
-	@define ( _news_photo_h , 70 );
-	@define ( _news_type , '<strong> .jpg|.gif|.jpg </strong>' );
-	
-	@define ( _product_thumb_w , 75 );
-	@define ( _product_thumb_h , 83 );
-	@define ( _product_photo_w , 300 );
-	@define ( _product_photo_h , 220 );
-	@define ( _product_type , '<strong> .jpg|.gif|.jpg</strong>' );
-	
-	@define ( _product_cat_photo_w , 200 );
-	@define ( _product_cat_photo_h , 140);
-	@define ( _product_cat_type , '<strong> .jpg|.gif|.jpg</strong>' );
+    
+    // ======================================= kich thuoc hinh anh
+    
+    @define(_news_thumb_w, 75);
+    @define(_news_thumb_h, 83);
+    @define(_news_photo_w, 120);
+    @define(_news_photo_h, 70);
+    @define(_news_type, '<strong> .jpg|.gif|.jpg </strong>');
+    
+    @define(_product_thumb_w, 75);
+    @define(_product_thumb_h, 83);
+    @define(_product_photo_w, 300);
+    @define(_product_photo_h, 220);
+    @define(_product_type, '<strong> .jpg|.gif|.jpg</strong>');
+    
+    @define(_product_cat_photo_w, 200);
+    @define(_product_cat_photo_h, 140);
+    @define(_product_cat_type, '<strong> .jpg|.gif|.jpg</strong>');
 
 
-	@define ( _client_thumb_w , 75 );
-	@define ( _client_thumb_h , 83 );
-	@define ( _client_photo_w , 120 );
-	@define ( _client_photo_h , 70 );
-	@define ( _client_type , '<strong> .jpg|.gif|.jpg</strong>' );
+    @define(_client_thumb_w, 75);
+    @define(_client_thumb_h, 83);
+    @define(_client_photo_w, 120);
+    @define(_client_photo_h, 70);
+    @define(_client_type, '<strong> .jpg|.gif|.jpg</strong>');
 
-	
-	@define ( _hinhanh_thumb_w , 117 );
-	@define ( _hinhanh_thumb_h , 88 );
-	@define ( _hinhanh_photo_w , 585 );
-	@define ( _hinhanh_photo_h , 440 );
-	@define ( _hinhanh_type , '<strong> .jpg|.gif|.jpg</strong>' );
-	
-	@define ( _hinhanh_item_photo_w , 585 );
-	@define ( _hinhanh_item_photo_h , 440 );
-	@define ( _hinhanh_item_type , '<strong> .jpg|.gif|.jpg</strong>' );
+    
+    @define(_hinhanh_thumb_w, 117);
+    @define(_hinhanh_thumb_h, 88);
+    @define(_hinhanh_photo_w, 585);
+    @define(_hinhanh_photo_h, 440);
+    @define(_hinhanh_type, '<strong> .jpg|.gif|.jpg</strong>');
+    
+    @define(_hinhanh_item_photo_w, 585);
+    @define(_hinhanh_item_photo_h, 440);
+    @define(_hinhanh_item_type, '<strong> .jpg|.gif|.jpg</strong>');
 
-	
-	@define ( _bannerqc_photo_w , 970 );
-	@define ( _bannerqc_photo_h , 150 );
-	@define ( _bannerqc_type , '<strong> .jpg|.gif|.jpg _ Width: '._bannerqc_photo_w.'px _ Height: '._bannerqc_photo_h.'px </strong>' );
-	
-	@define ( _logoqc_photo_w , 530 );
-	@define ( _logoqc_photo_h , 427);
-	@define ( _logo_type , '<strong> .jpg|.gif|.jpg _ Width: '._logoqc_photo_w.'px _ Height: '._logoqc_photo_h.'px </strong>' );
-	
-	
-	@define ( _banner_giua_photo_w , 950 );
-	@define ( _banner_giua_photo_h , 107);
-	@define ( _banner_giua_type , '<strong> .jpg|.gif|.jpg _ Width: '._banner_giua_photo_w.'px _ Height: '._banner_giua_photo_h.'px </strong>' );
+    
+    @define(_bannerqc_photo_w, 970);
+    @define(_bannerqc_photo_h, 150);
+    @define(_bannerqc_type, '<strong> .jpg|.gif|.jpg _ Width: '._bannerqc_photo_w.'px _ Height: '._bannerqc_photo_h.'px </strong>');
+    
+    @define(_logoqc_photo_w, 530);
+    @define(_logoqc_photo_h, 427);
+    @define(_logo_type, '<strong> .jpg|.gif|.jpg _ Width: '._logoqc_photo_w.'px _ Height: '._logoqc_photo_h.'px </strong>');
+    
+    
+    @define(_banner_giua_photo_w, 950);
+    @define(_banner_giua_photo_h, 107);
+    @define(_banner_giua_type, '<strong> .jpg|.gif|.jpg _ Width: '._banner_giua_photo_w.'px _ Height: '._banner_giua_photo_h.'px </strong>');
 
-	
-	
-	@define ( _doitac_photo_w , 125 );
-	@define ( _doitac_photo_h , 55 );
-	@define ( _doitac_type , '<strong> .jpg|.gif|.jpg _ Width: '._doitac_photo_w.'px _ Height: '._doitac_photo_h.'px </strong>' );
+    
+    
+    @define(_doitac_photo_w, 125);
+    @define(_doitac_photo_h, 55);
+    @define(_doitac_type, '<strong> .jpg|.gif|.jpg _ Width: '._doitac_photo_w.'px _ Height: '._doitac_photo_h.'px </strong>');
 
 ?>
